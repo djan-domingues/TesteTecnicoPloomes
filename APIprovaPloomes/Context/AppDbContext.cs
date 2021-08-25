9edca8ec95780c4a318c29a56b2212c35c29b579
Djan Domingues <felipej.domingues@gmail.com>
2021-08-24 23:47:26 -0300

    Primeira API desenvolvida

---
+++ APIprovaPloomes/Context/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using APIprovaPloomes.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIprovaPloomes.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ServerConnection"));
+        }
+    }
+}
---
+++ APIprovaPloomes/Controllers/ClienteController.cs
@@ -0,0 +1,47 @@
+﻿using APIprovaPloomes.Context;
+using APIprovaPloomes.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIprovaPloomes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ClienteController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClientes()
+        {
+            return Ok(new
+            {
+                succes = true,
+                data = await _appDbContext.Clientes.ToListAsync()
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCliente(Cliente cliente)
+        {
+            _appDbContext.Clientes.Add(cliente);
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(new
+            {
+                succes = true,
+                data = cliente
+            });
+        }
+
+    }
+}
